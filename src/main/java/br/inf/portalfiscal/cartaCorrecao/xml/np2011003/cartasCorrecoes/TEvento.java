bdec896402b50ddb69177666e8121b93a61fc24b
Murilo Tuvani <murilo.tuvani@gmail.com>
2025-04-08 15:27:22 -0300

    Estrutura do Maven

--- src/main/java/br/inf/portalfiscal/cartaCorrecao/xml/np2011003/cartasCorrecoes/TEvento.java
+++ src/main/java/br/inf/portalfiscal/cartaCorrecao/xml/np2011003/cartasCorrecoes/TEvento.java
@@ -86,7 +86,7 @@ import org.w3._2000._09.xmldsig.SignatureType;
  *                               &lt;simpleType>
  *                                 &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
  *                                   &lt;whiteSpace value="preserve"/>
- *                                   &lt;enumeration value="Carta de Correção"/>
+ *                                   &lt;enumeration value="Carta de CorreÃ§Ã£o"/>
  *                                   &lt;enumeration value="Carta de Correcao"/>
  *                                 &lt;/restriction>
  *                               &lt;/simpleType>
@@ -97,7 +97,7 @@ import org.w3._2000._09.xmldsig.SignatureType;
  *                                   &lt;whiteSpace value="preserve"/>
  *                                   &lt;minLength value="15"/>
  *                                   &lt;maxLength value="1000"/>
- *                                   &lt;pattern value="[!-ÿ]{1}[ -ÿ]{0,}[!-ÿ]{1}|[!-ÿ]{1}"/>
+ *                                   &lt;pattern value="[!-Ã¿]{1}[ -Ã¿]{0,}[!-Ã¿]{1}|[!-Ã¿]{1}"/>
  *                                 &lt;/restriction>
  *                               &lt;/simpleType>
  *                             &lt;/element>
@@ -105,7 +105,7 @@ import org.w3._2000._09.xmldsig.SignatureType;
  *                               &lt;simpleType>
  *                                 &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
  *                                   &lt;whiteSpace value="preserve"/>
- *                                   &lt;enumeration value="A Carta de Correção é disciplinada pelo § 1º-A do art. 7º do Convênio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularização de erro ocorrido na emissão de documento fiscal, desde que o erro não esteja relacionado com: I - as variáveis que determinam o valor do imposto tais como: base de cálculo, alíquota, diferença de preço, quantidade, valor da operação ou da prestação; II - a correção de dados cadastrais que implique mudança do remetente ou do destinatário; III - a data de emissão ou de saída."/>
+ *                                   &lt;enumeration value="A Carta de CorreÃ§Ã£o Ã© disciplinada pelo Â§ 1Âº-A do art. 7Âº do ConvÃªnio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularizaÃ§Ã£o de erro ocorrido na emissÃ£o de documento fiscal, desde que o erro nÃ£o esteja relacionado com: I - as variÃ¡veis que determinam o valor do imposto tais como: base de cÃ¡lculo, alÃ­quota, diferenÃ§a de preÃ§o, quantidade, valor da operaÃ§Ã£o ou da prestaÃ§Ã£o; II - a correÃ§Ã£o de dados cadastrais que implique mudanÃ§a do remetente ou do destinatÃ¡rio; III - a data de emissÃ£o ou de saÃ­da."/>
  *                                   &lt;enumeration value="A Carta de Correcao e disciplinada pelo paragrafo 1o-A do art. 7o do Convenio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularizacao de erro ocorrido na emissao de documento fiscal, desde que o erro nao esteja relacionado com: I - as variaveis que determinam o valor do imposto tais como: base de calculo, aliquota, diferenca de preco, quantidade, valor da operacao ou da prestacao; II - a correcao de dados cadastrais que implique mudanca do remetente ou do destinatario; III - a data de emissao ou de saida."/>
  *                                 &lt;/restriction>
  *                               &lt;/simpleType>
@@ -292,7 +292,7 @@ public class TEvento {
      *                     &lt;simpleType>
      *                       &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
      *                         &lt;whiteSpace value="preserve"/>
-     *                         &lt;enumeration value="Carta de Correção"/>
+     *                         &lt;enumeration value="Carta de CorreÃ§Ã£o"/>
      *                         &lt;enumeration value="Carta de Correcao"/>
      *                       &lt;/restriction>
      *                     &lt;/simpleType>
@@ -303,7 +303,7 @@ public class TEvento {
      *                         &lt;whiteSpace value="preserve"/>
      *                         &lt;minLength value="15"/>
      *                         &lt;maxLength value="1000"/>
-     *                         &lt;pattern value="[!-ÿ]{1}[ -ÿ]{0,}[!-ÿ]{1}|[!-ÿ]{1}"/>
+     *                         &lt;pattern value="[!-Ã¿]{1}[ -Ã¿]{0,}[!-Ã¿]{1}|[!-Ã¿]{1}"/>
      *                       &lt;/restriction>
      *                     &lt;/simpleType>
      *                   &lt;/element>
@@ -311,7 +311,7 @@ public class TEvento {
      *                     &lt;simpleType>
      *                       &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
      *                         &lt;whiteSpace value="preserve"/>
-     *                         &lt;enumeration value="A Carta de Correção é disciplinada pelo § 1º-A do art. 7º do Convênio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularização de erro ocorrido na emissão de documento fiscal, desde que o erro não esteja relacionado com: I - as variáveis que determinam o valor do imposto tais como: base de cálculo, alíquota, diferença de preço, quantidade, valor da operação ou da prestação; II - a correção de dados cadastrais que implique mudança do remetente ou do destinatário; III - a data de emissão ou de saída."/>
+     *                         &lt;enumeration value="A Carta de CorreÃ§Ã£o Ã© disciplinada pelo Â§ 1Âº-A do art. 7Âº do ConvÃªnio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularizaÃ§Ã£o de erro ocorrido na emissÃ£o de documento fiscal, desde que o erro nÃ£o esteja relacionado com: I - as variÃ¡veis que determinam o valor do imposto tais como: base de cÃ¡lculo, alÃ­quota, diferenÃ§a de preÃ§o, quantidade, valor da operaÃ§Ã£o ou da prestaÃ§Ã£o; II - a correÃ§Ã£o de dados cadastrais que implique mudanÃ§a do remetente ou do destinatÃ¡rio; III - a data de emissÃ£o ou de saÃ­da."/>
      *                         &lt;enumeration value="A Carta de Correcao e disciplinada pelo paragrafo 1o-A do art. 7o do Convenio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularizacao de erro ocorrido na emissao de documento fiscal, desde que o erro nao esteja relacionado com: I - as variaveis que determinam o valor do imposto tais como: base de calculo, aliquota, diferenca de preco, quantidade, valor da operacao ou da prestacao; II - a correcao de dados cadastrais que implique mudanca do remetente ou do destinatario; III - a data de emissao ou de saida."/>
      *                       &lt;/restriction>
      *                     &lt;/simpleType>
@@ -663,7 +663,7 @@ public class TEvento {
          *           &lt;simpleType>
          *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
          *               &lt;whiteSpace value="preserve"/>
-         *               &lt;enumeration value="Carta de Correção"/>
+         *               &lt;enumeration value="Carta de CorreÃ§Ã£o"/>
          *               &lt;enumeration value="Carta de Correcao"/>
          *             &lt;/restriction>
          *           &lt;/simpleType>
@@ -674,7 +674,7 @@ public class TEvento {
          *               &lt;whiteSpace value="preserve"/>
          *               &lt;minLength value="15"/>
          *               &lt;maxLength value="1000"/>
-         *               &lt;pattern value="[!-ÿ]{1}[ -ÿ]{0,}[!-ÿ]{1}|[!-ÿ]{1}"/>
+         *               &lt;pattern value="[!-Ã¿]{1}[ -Ã¿]{0,}[!-Ã¿]{1}|[!-Ã¿]{1}"/>
          *             &lt;/restriction>
          *           &lt;/simpleType>
          *         &lt;/element>
@@ -682,7 +682,7 @@ public class TEvento {
          *           &lt;simpleType>
          *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
          *               &lt;whiteSpace value="preserve"/>
-         *               &lt;enumeration value="A Carta de Correção é disciplinada pelo § 1º-A do art. 7º do Convênio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularização de erro ocorrido na emissão de documento fiscal, desde que o erro não esteja relacionado com: I - as variáveis que determinam o valor do imposto tais como: base de cálculo, alíquota, diferença de preço, quantidade, valor da operação ou da prestação; II - a correção de dados cadastrais que implique mudança do remetente ou do destinatário; III - a data de emissão ou de saída."/>
+         *               &lt;enumeration value="A Carta de CorreÃ§Ã£o Ã© disciplinada pelo Â§ 1Âº-A do art. 7Âº do ConvÃªnio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularizaÃ§Ã£o de erro ocorrido na emissÃ£o de documento fiscal, desde que o erro nÃ£o esteja relacionado com: I - as variÃ¡veis que determinam o valor do imposto tais como: base de cÃ¡lculo, alÃ­quota, diferenÃ§a de preÃ§o, quantidade, valor da operaÃ§Ã£o ou da prestaÃ§Ã£o; II - a correÃ§Ã£o de dados cadastrais que implique mudanÃ§a do remetente ou do destinatÃ¡rio; III - a data de emissÃ£o ou de saÃ­da."/>
          *               &lt;enumeration value="A Carta de Correcao e disciplinada pelo paragrafo 1o-A do art. 7o do Convenio S/N, de 15 de dezembro de 1970 e pode ser utilizada para regularizacao de erro ocorrido na emissao de documento fiscal, desde que o erro nao esteja relacionado com: I - as variaveis que determinam o valor do imposto tais como: base de calculo, aliquota, diferenca de preco, quantidade, valor da operacao ou da prestacao; II - a correcao de dados cadastrais que implique mudanca do remetente ou do destinatario; III - a data de emissao ou de saida."/>
          *             &lt;/restriction>
          *           &lt;/simpleType>
